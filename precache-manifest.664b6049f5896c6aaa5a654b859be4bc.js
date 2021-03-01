9db35515a5b0612ca5a52e50c43ec8a979355bc5
Ivan <i.romanov@protonmail.com>
2021-03-01 19:04:09 +0300

    Updates

---
+++ precache-manifest.664b6049f5896c6aaa5a654b859be4bc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7214c6b42c994312a6357ddef43270de",
+    "url": "/wincom/index.html"
+  },
+  {
+    "revision": "9d6750291dfca345ccb1",
+    "url": "/wincom/static/css/2.2cba102f.chunk.css"
+  },
+  {
+    "revision": "38453798c6c8784d6f43",
+    "url": "/wincom/static/css/main.2d50fdcc.chunk.css"
+  },
+  {
+    "revision": "9d6750291dfca345ccb1",
+    "url": "/wincom/static/js/2.d6b31f05.chunk.js"
+  },
+  {
+    "revision": "5b099948b207b77f6fa04ff33fa98702",
+    "url": "/wincom/static/js/2.d6b31f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38453798c6c8784d6f43",
+    "url": "/wincom/static/js/main.aeee42aa.chunk.js"
+  },
+  {
+    "revision": "6d81dce4baab94fe95e6",
+    "url": "/wincom/static/js/runtime-main.21b6fed9.js"
+  },
+  {
+    "revision": "72059a18e01dcfdddfc1b8b1d5f58e73",
+    "url": "/wincom/static/media/OpenSans-Bold.72059a18.woff2"
+  },
+  {
+    "revision": "737e96cadf4a01fcd3a67e09c3ebc86a",
+    "url": "/wincom/static/media/OpenSans-Regular.737e96ca.woff2"
+  },
+  {
+    "revision": "c9fde9681cba560dc4661dad021298c0",
+    "url": "/wincom/static/media/OpenSans-SemiBold.c9fde968.woff2"
+  },
+  {
+    "revision": "1f5404506a1637b2a2f731a493a38134",
+    "url": "/wincom/static/media/Raleway-Black.1f540450.woff2"
+  },
+  {
+    "revision": "b2d08bfc7b92e641ec9335b4e9fb2e7f",
+    "url": "/wincom/static/media/Raleway-Bold.b2d08bfc.woff2"
+  },
+  {
+    "revision": "b56d0bd94cd4b476a16f50bcdd69c713",
+    "url": "/wincom/static/media/Raleway-Light.b56d0bd9.woff2"
+  },
+  {
+    "revision": "856962b71c713e3304840e9091fe80b9",
+    "url": "/wincom/static/media/Raleway-Medium.856962b7.woff2"
+  },
+  {
+    "revision": "00276f45cbff23ef73a2cd2ebd0c9681",
+    "url": "/wincom/static/media/Raleway-Regular.00276f45.woff2"
+  },
+  {
+    "revision": "55508842f3c9af4ac101bf351e3a40e3",
+    "url": "/wincom/static/media/Raleway-SemiBold.55508842.woff2"
+  },
+  {
+    "revision": "09d6db7d3a908ea9df2f0365fcc3de74",
+    "url": "/wincom/static/media/icon-arrow-calendar.09d6db7d.svg"
+  },
+  {
+    "revision": "52d9ffd9f1ec9dd674c36a9fc376eaee",
+    "url": "/wincom/static/media/license.52d9ffd9.png"
+  },
+  {
+    "revision": "defeb78ed25021fc3276be536a057b1e",
+    "url": "/wincom/static/media/truck-dummy.defeb78e.jpg"
+  }
+]);
